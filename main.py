018b77ec630ba3e648ebc1ac6bfb4df4d08292b1
Havard <havard.line@lts.no>
2017-07-03 10:07:53 +0200

    testing automat library

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.ui import Icon, Menu
 import subprocess
 from PIL import Image
 from pystray import MenuItem
+from automat import MethodicalMachine
 
 user_key = None
 state=False
@@ -12,6 +13,68 @@ crypto_tools = tools(userdir.crypto)
 api_tools = connection()
 icon = Icon()
 
+
+#============================ state machine start
+class State_machine(object):
+    _machine = MethodicalMachine()
+    _beans = ""
+    
+#============================ inputs
+    @_machine.input()
+    def initiate_connection(self):
+        "Initiate connection to server"
+
+    @_machine.input()
+    def put_in_beans(self, beans):
+        "The user put in some beans."
+
+#============================ outputs
+    @_machine.output()
+    def _heat_the_heating_element(self):
+        "Heat up the heating element, which should cause coffee to happen."
+        print("heating turned on")
+
+    @_machine.output()
+    def _save_beans(self, beans):
+        "The beans are now in the machine; save them."
+        self._beans = beans
+
+    @_machine.output()
+    def _describe_coffee(self):
+        return "A cup of coffee made with {}.".format(self._beans)
+
+#============================ states
+    @_machine.state()
+    def have_beans(self):
+        "In this state, you have some beans."
+        
+    @_machine.state(initial=True)
+    def dont_have_beans(self):
+        "In this state, you don't have any beans."
+        
+
+#============================ transitions
+    # When we don't have beans, upon putting in beans, we will then have beans
+    dont_have_beans.upon(
+        put_in_beans,
+        enter=have_beans,
+        outputs=[
+            _save_beans
+            ]
+        )
+
+    # When we have beans, upon pressing the brew button, we will then not have
+    # beans any more (as they have been entered into the brewing chamber) and
+    have_beans.upon(
+        brew_button, enter=dont_have_beans,
+        outputs=[
+            _heat_the_heating_element,
+            _describe_coffee
+            ],
+        collector=lambda iterable: list(iterable)[-1]
+        )
+#============================ state machine end
+    
 def on_click_available(icon):
     print('UI event: click')
     global state
@@ -63,4 +126,7 @@ def setup(icon):
 
     
 if __name__=="__main__":
-    icon.run(setup)
+    #icon.run(setup)
+    coffee_machine = State_machine()
+    coffee_machine.put_in_beans("real good beans")
+    print(coffee_machine.brew_button())
